5cbb71d3aa7385950b446a3089b2eb38a035c130
Jason Kuang <jason.jkuang@gmail.com>
2015-05-27 15:09:03 -0700

    numbers-and-variables-assignment

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -12,17 +12,21 @@
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
-    return number;
+//    NSInteger sourceNumber = number;
+//    sourceNumber++;
+    return ++number;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
-    return number1;
+    NSInteger result = number1 + number2;
+    return result;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    NSInteger result = dividend % divisor;
+    return result;
 }
 
 @end
